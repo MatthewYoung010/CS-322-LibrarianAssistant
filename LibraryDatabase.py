72f2475bc71a4d4a20455506f9711fec857b30da
MatthewYoung010 <matthewyoung010@gmail.com>
2025-03-22 16:31:07 -0700

    Library Database v1.01
    Final version of list version of database before the actual database is used.

--- LibraryDatabase.py
+++ LibraryDatabase.py
@@ -1,9 +1,14 @@
 #import sqlite3
+#Will be replacing the list with a database in the next version
 
-class Book:
-    def __init__(self, title, author, serialNum):
+class LogicalBook:
+    """Books won't have their each unique copy. Instead there is one larger logical book
+    and if there are more than one copy of said book the copies would be two. Serial Number
+    is the unique identification for each logical book."""
+    def __init__(self, title, author, copies, serialNum):
         self.title = title
         self.author = author
+        self.copies = copies
         self.serialNum = serialNum
     
     def changeTitle(self, newTitle):
@@ -12,21 +17,89 @@ class Book:
     def changeAuthor(self, newAuthor):
         self.author = newAuthor
     
-    def changeTitle(self, newSerialNum):
+    def changeNumOfCopies(self, newCopiesNum):
+        self.copies = newCopiesNum
+
+    def changeSerialNum(self, newSerialNum):
         self.serialNum = newSerialNum
 
-class Database:
+class Catalog:
+    """Catalog holds all of the functions for editing a catalog."""
     def __init__(self, InitalBook):
         self.catalog = [InitalBook]
 
-    def addBook(self, newBook):
-        self.catalog.append(newBook)
-    
-    def printCatalog(self):
-        print("Title        Author      Serial Number")
-        for Catalog in self.catalog:
-            print(f"{Catalog.title}     {Catalog.author}    {Catalog.serialNum}")
 
+    def addBook(self, newBook):
+        """Add a book to the catalog. If there already exists a copy of the book in the catalog 
+        it will add the amount of copies in the object instead."""
+
+        doesBookExist = self.doesBookExist(newBook.serialNum)
+        if(doesBookExist == True):
+            self.findBookBySerialNum(newBook.serialNum).changeNumOfCopies(self.findBookBySerialNum(newBook.serialNum).copies + newBook.copies)
+        else:
+            self.catalog.append(newBook)
+
+        
+    
+    def removeBook(self, bookToRemove):
+        """Removes books based on how many copies the object has."""
+        doesBookExist = self.findBookBySerialNum(bookToRemove.serialNum)
+        if(doesBookExist.copies - bookToRemove.copies > 1):
+            doesBookExist.changeNumOfCopies(doesBookExist.copies - bookToRemove.copies)
+        else:
+            self.catalog.remove(bookToRemove)
+    
+    def editBook (self,newTitle, newAuthor, SerialNum):
+       """Edits the Title and Author of a book"""
+       if(self.doesBookExist(SerialNum) == True):
+            BookToEdit = self.findBookBySerialNum(SerialNum)
+
+            BookToEdit.changeTitle(newTitle)
+            BookToEdit.changeAuthor(newAuthor)
+           
+      
+       
+       
+
+    def printCatalog(self):
+        """Self Explanatory Print Function."""
+        print("Title        Author      Serial Number")
+        for Book in self.catalog:
+            print(f"{Book.title}     {Book.author}    {Book.copies}     {Book.serialNum}")
+    
+    def findBookBySerialNum(self, serialNumOfBook):
+        """Search for a book in the catalog by serial number. Returns the one unique book."""
+        for Book in self.catalog:
+            if(serialNumOfBook == Book.serialNum):
+                #print("Book Found")
+                return Book
+            
+    def doesBookExist(self, serialNumOfBook):
+        """Search for a book in the catalog by serial number. Returns a boolean"""
+        for Book in self.catalog:
+            if(serialNumOfBook == Book.serialNum):
+                #print("Book Found")
+                return True
+            
+        return False
+            
+    def findBookByAuthor(self, authorTarget):
+        """Search for a book in the catalog by author. Can return multiple."""
+        matchedBooks = list()
+        for Book in self.catalog:
+            if(authorTarget == Book.author):
+                matchedBooks.append(Book)
+
+        return matchedBooks 
+
+    def findBookByTitle(self, titleTarget):
+        """Search for a book in the catalog by title. Can return multiple."""
+        matchedBooks = list()
+        for Book in self.catalog:
+            if(titleTarget == Book.title):
+                matchedBooks.append(Book)  
+
+        return matchedBooks 
 
 
 #def connectToLibraryDatabase():
--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 from LibraryDatabase import *
 
 
-Wikipeida = Book("Wikipeida", "John Doe", 90000001)
-Mistborn = Book("Mistborn", "Brandon Sanderson", 90000002)
-PythonForDumDums = Book("Python For Dum Dum's", "Some nerd", 90000003)
+Wikipeida = LogicalBook("Wikipeida", "John Doe",2, 90000001)
+Mistborn = LogicalBook("Mistborn", "Brandon Sanderson",3, 90000002)
+PythonForDumDums = LogicalBook("Python For Dum Dum's", "Some nerd",1, 90000003)
 
-LibraryCatalog = Database(Wikipeida)
+LibraryCatalog = Catalog(Wikipeida)
 LibraryCatalog.addBook(Mistborn)
 LibraryCatalog.addBook(PythonForDumDums)
 
 LibraryCatalog.printCatalog()
 
+print("Enter the Serial Number of the book to edit:")
+serialNum = int(input())
 
+print("What is the new title?")
+newTitle = input()
+
+print("What is the new author?")
+newAuthor = input()
+
+LibraryCatalog.editBook(newTitle, newAuthor, serialNum)
+
+
+LibraryCatalog.printCatalog()
+
+AnotherCopyOfMistborn = LogicalBook("Mistborn", "Brandon Sanderson", 1, 90000002)
+LibraryCatalog.addBook(AnotherCopyOfMistborn)
+Heros = LogicalBook("Heros","Brandon Sanderson", 1, 90000004)
+LibraryCatalog.addBook(Heros)
+
+print("Title        Author      Number Of Copies        Serial Number")
+for Book in LibraryCatalog.findBookByAuthor("Brandon Sanderson"):
+     print(f"{Book.title}     {Book.author}    {Book.copies}        {Book.serialNum}")
+
+
+print("Enter the Serial Number of the book to edit:")
+serialNum = int(input())
+
+print("What is the new title?")
+newTitle = input()
+
+print("What is the new author?")
+newAuthor = input()
+
+LibraryCatalog.editBook(newTitle, newAuthor, serialNum)
+LibraryCatalog.printCatalog()
